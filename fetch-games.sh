7ba5ae79e391089411fbbb74a52e74cf3806427f
Nick Hollon <falling.masonry@gmail.com>
2021-09-14 13:26:22 -0700

    curl command for fetching rated games from Lichess

---
+++ fetch-games.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+LICHESS_API_TOKEN=`cat .api-token`
+curl "https://lichess.org/api/games/user/pigeonpal?rated=true&perfType=ultraBullet,bullet,blitz,rapid,classical,correspondence&pgnInJson=true&clocks=true&opening=true" -H "Authorization: Bearer $LICHESS_API_TOKEN" -H "Accept: application/x-ndjson"
