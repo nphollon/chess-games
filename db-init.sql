ceffeececf5316109d67ee8db62443fd147a3ba4
Nick Hollon <nick@everlaw.com>
2021-09-20 13:30:55 -0700

    Remove player table; rename table game_format=>time_format

--- db-init.sql
+++ db-init.sql
@@ -1,16 +1,4 @@
-CREATE TABLE player
-( id INTEGER AUTO_INCREMENT PRIMARY KEY
-, lichess_id VARCHAR(40) UNIQUE
-, name VARCHAR(40)
-, rating SMALLINT UNSIGNED
-, provisional TINYINT(1)
-, lifetime_wins_against SMALLINT UNSIGNED
-, lifetime_losses_to SMALLINT UNSIGNED
-, lifetime_draws SMALLINT UNSIGNED
-);
-
-
-CREATE TABLE game_format
+CREATE TABLE IF NOT EXISTS time_format
 ( id INTEGER AUTO_INCREMENT PRIMARY KEY
 , speed VARCHAR(40) NOT NULL
 , time SMALLINT UNSIGNED NOT NULL
@@ -18,7 +6,7 @@ CREATE TABLE game_format
 );
 
 
-CREATE TABLE opening
+CREATE TABLE IF NOT EXISTS opening
 ( id INTEGER AUTO_INCREMENT PRIMARY KEY
 , eco VARCHAR(3) UNIQUE NOT NULL
 , name VARCHAR(120) NOT NULL
@@ -26,37 +14,31 @@ CREATE TABLE opening
 );
 
 
-CREATE TABLE game
+CREATE TABLE IF NOT EXISTS game
 ( id INTEGER AUTO_INCREMENT PRIMARY KEY
 , lichess_id VARCHAR(8) UNIQUE
-, game_number INTEGER UNSIGNED
 , datetime DATETIME
 , color ENUM('white','black')
-
 , format_id INTEGER
-, CONSTRAINT FOREIGN KEY (format_id) REFERENCES game_format(id) ON DELETE RESTRICT
-
-, opponent_id INTEGER
-, CONSTRAINT FOREIGN KEY (opponent_id) REFERENCES player(id) ON DELETE RESTRICT
-
 , result ENUM('win','loss','draw')
 , result_detail VARCHAR(80)
 , rating SMALLINT UNSIGNED
 , provisional TINYINT(1)
 , rating_change SMALLINT
+, opponent_name VARCHAR(80)
 , opponent_rating SMALLINT UNSIGNED
-
 , opening_id INTEGER
-, CONSTRAINT FOREIGN KEY (opening_id) REFERENCES opening(id) ON DELETE RESTRICT
-
 , time_remaining TIME
 , ply_length SMALLINT UNSIGNED
 , moves TEXT
 , pgn TEXT
+
+, CONSTRAINT FOREIGN KEY (format_id) REFERENCES time_format(id)
+, CONSTRAINT FOREIGN KEY (opening_id) REFERENCES opening(id)
 );
 
 
-CREATE TABLE analysis
+CREATE TABLE IF NOT EXISTS analysis
 ( id INTEGER AUTO_INCREMENT PRIMARY KEY
 
 , game_id INTEGER NOT NULL
